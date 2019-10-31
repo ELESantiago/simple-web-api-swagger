6e114f3f5ead65eb0a914c1280dd2fd2ee478d3f
santiago_lsvm@outlook.com <santiago_lsvm@outlook.com>
2019-10-31 06:16:44 -0600

    Initial commit

---
+++ WebApiTemplate/Controllers/ExampleController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTemplate.Controllers
+{
+    [Produces("application/json")]
+    [ApiConventionType(typeof(DefaultApiConventions))] // with this there is not necessary to apply it to each method
+    [ApiController]
+    [Route("[controller]")]
+    public class ExampleController: ControllerBase
+    {
+        /// <summary>
+        /// Get all examples
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Get))]
+        public ActionResult<IEnumerable<Example>> GetAllExamples()
+        {
+            return Ok(Examples.ExampleList);
+        }
+
+        /// <summary>
+        /// Create a new example
+        /// </summary>
+        /// <param name="example"></param>
+        /// <returns>A string with the word success</returns>
+        /// <response code="201">Returns success</response>
+        /// <response code="400">If something goes wrong</response>
+        [HttpPost]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Post))]
+        public ActionResult<string> CreateExample([FromBody] Example example)
+        {
+            return CreatedAtRoute("GetExample", new { id = 1 }, "Success");
+        }
+
+        /// <summary>
+        /// Get an Example by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>The corresponding example object</returns>
+        /// <response code="201">Returns success</response>
+        /// <response code="400">If something goes wrong</response>
+        /// <response code="404">If the id doesn't correspond to any registry</response>
+        [HttpGet("{id}")]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Get))]
+        public ActionResult<Example> GetExample(int id)
+        {
+            var examples = Examples.ExampleList.ToList();
+            var example = examples.Find(e => e.Id == id);
+            if (example == null)
+            {
+                return NotFound();
+            }
+            return Ok(example);
+        }
+    }
+}
---
+++ WebApiTemplate/Example.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTemplate
+{
+    public class Example
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DefaultValue(false)]
+        public bool IsDeleted { get; set; }
+        public DateTime CreationTime { get; set; }
+
+    }
+
+
+    public static class Examples
+    {
+        public static IEnumerable<Example> ExampleList = new Example[]
+        {
+            new Example { Id = 1, Name = "First Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 2, Name = "Second Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 3, Name = "Third Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 4, Name = "Fourth Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 5, Name = "Fifth Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 6, Name = "Sixth Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 7, Name = "Seventh Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 8, Name = "Eighth Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 9, Name = "Nineth Example", CreationTime = DateTime.Now, IsDeleted = false },
+            new Example { Id = 10, Name = "Tenth Example", CreationTime = DateTime.Now, IsDeleted = false },
+        };
+    }
+}
